d88e5c599ae301f38509e161bfce65a3567bd7cf
Carson Angell <carsonjesseangell@gmail.com>
2026-01-20 23:22:15 -0700

    Added a development test class for me to try random things while developing, will be removed when finished

---
+++ FormulaTests/DevTests.cs
@@ -0,0 +1,12 @@
+//TODO - Delete this class when assignment is finished
+namespace FormulaTests;
+
+using Formula;
+
+[TestClass]
+public class DevTests {
+    [TestMethod]
+    public void TestMethod1() {
+        
+    }
+}
