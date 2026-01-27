00a90e184d2be3b9c0c86bfc948004e6bd7b491b
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-01-27 10:53:22 -0700

    Formatted unit test code for DependencyGraph

--- Spreadsheet/DependencyGraphTests/DependencyGraphTests.cs
+++ Spreadsheet/DependencyGraphTests/DependencyGraphTests.cs
@@ -7,84 +7,74 @@ using DependencyGraph;
 ///   to contain all DependencyGraphTest Unit Tests
 /// </summary>
 [TestClass]
-public class DependencyGraphTests
-{
-  /// <summary>
-  ///   TODO:  Explain carefully what this code tests.
-  ///          Also, update in-line comments as appropriate.
-  /// </summary>
-  [TestMethod]
-  [Timeout( 2000, CooperativeCancellation = true)]  // 2 second run time limit
-  public void StressTest()
-  {
-    DependencyGraph dg = new();
+public class DependencyGraphTests {
 
-    // A bunch of strings to use
-    const int SIZE = 200;
-    string[] letters = new string[SIZE];
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      letters[i] = string.Empty + ( (char) ( 'a' + i ) );
-    }
 
-    // The correct answers
-    HashSet<string>[] dependents = new HashSet<string>[SIZE];
-    HashSet<string>[] dependees = new HashSet<string>[SIZE];
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      dependents[i] = [];
-      dependees[i] = [];
-    }
 
-    // Add a bunch of dependencies
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      for ( int j = i + 1; j < SIZE; j++ )
-      {
-        dg.AddDependency( letters[i], letters[j] );
-        dependents[i].Add( letters[j] );
-        dependees[j].Add( letters[i] );
-      }
-    }
+    /// <summary>
+    ///   TODO:  Explain carefully what this code tests.
+    ///          Also, update in-line comments as appropriate.
+    /// </summary>
+    [TestMethod]
+    [Timeout(2000, CooperativeCancellation = true)]  // 2 second run time limit
+    public void StressTest() {
+        DependencyGraph dg = new();
 
-    // Remove a bunch of dependencies
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      for ( int j = i + 4; j < SIZE; j += 4 )
-      {
-        dg.RemoveDependency( letters[i], letters[j] );
-        dependents[i].Remove( letters[j] );
-        dependees[j].Remove( letters[i] );
-      }
-    }
+        // A bunch of strings to use
+        const int SIZE = 200;
+        string[] letters = new string[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            letters[i] = string.Empty + ((char)('a' + i));
+        }
 
-    // Add some back
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      for ( int j = i + 1; j < SIZE; j += 2 )
-      {
-        dg.AddDependency( letters[i], letters[j] );
-        dependents[i].Add( letters[j] );
-        dependees[j].Add( letters[i] );
-      }
-    }
+        // The correct answers
+        HashSet<string>[] dependents = new HashSet<string>[SIZE];
+        HashSet<string>[] dependees = new HashSet<string>[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            dependents[i] = [];
+            dependees[i] = [];
+        }
 
-    // Remove some more
-    for ( int i = 0; i < SIZE; i += 2 )
-    {
-      for ( int j = i + 3; j < SIZE; j += 3 )
-      {
-        dg.RemoveDependency( letters[i], letters[j] );
-        dependents[i].Remove( letters[j] );
-        dependees[j].Remove( letters[i] );
-      }
-    }
+        // Add a bunch of dependencies
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = i + 1; j < SIZE; j++) {
+                dg.AddDependency(letters[i], letters[j]);
+                dependents[i].Add(letters[j]);
+                dependees[j].Add(letters[i]);
+            }
+        }
 
-    // Make sure everything is right
-    for ( int i = 0; i < SIZE; i++ )
-    {
-      Assert.IsTrue( dependents[i].SetEquals( new HashSet<string>( dg.GetDependents( letters[i] ) ) ) );
-      Assert.IsTrue( dependees[i].SetEquals( new HashSet<string>( dg.GetDependees( letters[i] ) ) ) );
+        // Remove a bunch of dependencies
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = i + 4; j < SIZE; j += 4) {
+                dg.RemoveDependency(letters[i], letters[j]);
+                dependents[i].Remove(letters[j]);
+                dependees[j].Remove(letters[i]);
+            }
+        }
+
+        // Add some back
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = i + 1; j < SIZE; j += 2) {
+                dg.AddDependency(letters[i], letters[j]);
+                dependents[i].Add(letters[j]);
+                dependees[j].Add(letters[i]);
+            }
+        }
+
+        // Remove some more
+        for (int i = 0; i < SIZE; i += 2) {
+            for (int j = i + 3; j < SIZE; j += 3) {
+                dg.RemoveDependency(letters[i], letters[j]);
+                dependents[i].Remove(letters[j]);
+                dependees[j].Remove(letters[i]);
+            }
+        }
+
+        // Make sure everything is right
+        for (int i = 0; i < SIZE; i++) {
+            Assert.IsTrue(dependents[i].SetEquals(new HashSet<string>(dg.GetDependents(letters[i]))));
+            Assert.IsTrue(dependees[i].SetEquals(new HashSet<string>(dg.GetDependees(letters[i]))));
+        }
     }
-  }
 }
