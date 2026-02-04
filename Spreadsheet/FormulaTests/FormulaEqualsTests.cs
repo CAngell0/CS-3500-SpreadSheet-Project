214c1c8e1eecd652eb2fe766c352990f1f9f3b91
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-04 15:51:54 -0700

    Added unit tests for equal single token formulas for the Equals method

--- Spreadsheet/FormulaTests/FormulaEqualsTests.cs
+++ Spreadsheet/FormulaTests/FormulaEqualsTests.cs
@@ -1,8 +1,48 @@
 namespace FormulaTests;
 
+using Formula;
+
 [TestClass]
 public class FormulaEqualsTests {
+
+    // --- TESTS USING THE .Equals METHOD ---
+    // - Tests with single token formulas that are equal -
+
     [TestMethod]
-    public void TestMethod1() {
+    public void FormulaEqualsMethod_EqualSingleIntegerTokenFormulas_Equal() {
+        Formula f1 = new("89");
+        Formula f2 = new("89");
+
+        Assert.IsTrue(f1.Equals(f2));
+    }
+
+    [TestMethod]
+    public void FormulaEqualsMethod_EqualSingleDecimalTokenFormulas_Equal() {
+        Formula f1 = new("32.45");
+        Formula f2 = new("32.45");
+
+        Assert.IsTrue(f1.Equals(f2));
+    }
+
+    [TestMethod]
+    public void FormulaEqualsMethod_EqualSingleScientificTokenFormulas_Equal() {
+        Formula f1 = new("5e2");
+        Formula f2 = new("500");
+        Formula f3 = new("5E2");
+
+        Assert.IsTrue(f1.Equals(f2));
+        Assert.IsTrue(f2.Equals(f3));
+        Assert.IsTrue(f3.Equals(f1));
+    }
+
+    [TestMethod]
+    public void FormulaEqualsMethod_EqualSingleVariableTokenFormulas_Equal() {
+        Formula f1 = new("a15");
+        Formula f2 = new("A15");
+        Formula f3 = new("A15");
+
+        Assert.IsTrue(f1.Equals(f2));
+        Assert.IsTrue(f2.Equals(f3));
+        Assert.IsTrue(f3.Equals(f1));
     }
 }
