b317268cc0311b0c84e6685288b98246c29535c5
Carson Angell <carsonjesseangell@gmail.com>
2026-02-06 10:53:01 -0700

    Added unit tests with inequal dual token formulas, with and without parentheses

--- Spreadsheet/FormulaTests/FormulaEquivilencyTests.cs
+++ Spreadsheet/FormulaTests/FormulaEquivilencyTests.cs
@@ -157,6 +157,17 @@ public class FormulaEqualsTests {
         }
     }
 
+    [TestMethod]
+    public void FormulaEquivilency_EqualMixedDualTokenFormulas_Equal() {
+        Formula[] testedFormulas = [new("ab12 / .54"), new("AB012/ 0.540"), new("aB12 / 5.4e-1")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsTrue(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsTrue(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsFalse(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
 
 
 
@@ -196,6 +207,22 @@ public class FormulaEqualsTests {
     [TestMethod]
     public void FormulaEquivilency_EqualDualVariableTokenFormulasWithParens_Equal() {
         Formula[] testedFormulas = [new("(( (a15 - zc29)  ))"), new("(( (A15-Zc29)  ))"), new("(((A15-zC29)))"), new("( ( (a15-ZC29)  ))")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsTrue(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsTrue(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsFalse(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_EqualMixedDualTokenFormulasWithParens_Equal() {
+        Formula[] testedFormulas = [
+            new("( ((5632.0) / 2e3  ))"), 
+            new("(((56.32e2)/ 2000) )"), 
+            new("( ( (5632000e-3) / 20E2) )"),
+            new("(((5632) / 02000))")
+        ];
 
         for (int i = 0; i < testedFormulas.Length; i++) {
             Assert.IsTrue(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
@@ -329,6 +356,127 @@ public class FormulaEqualsTests {
     [TestMethod]
     public void FormulaEquivilency_InequalSingleVariableTokenFormulasWithParens_Inequal() {
         Formula[] testedFormulas = [new("((a152))"), new("( A15   )"), new("(   ( A15 )   )")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+
+
+
+    // - Tests with two token formulas that are not equal -
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualIntegerTokenFormulas_Inequal() {
+        Formula f1 = new("89 + 321");
+        Formula f2 = new("89 + 32");
+
+        Assert.IsFalse(f1.Equals(f2));
+        Assert.IsFalse(f1 == f2);
+        Assert.IsTrue(f1 != f2);
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualDecimalTokenFormulas_Inequal() {
+        Formula f1 = new("32.45 - .2e1");
+        Formula f2 = new("32.045 - .2");
+
+        Assert.IsFalse(f1.Equals(f2));
+        Assert.IsFalse(f1 == f2);
+        Assert.IsTrue(f1 != f2);
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualScientificTokenFormulas_Inequal() {
+        Formula[] testedFormulas = [new("5e2 / .3e1"), new("500 / 30"), new("5E2/3E2")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualVariableTokenFormulas_Inequal() {
+        Formula[] testedFormulas = [new("a0151 - zc29"), new("A15*Zc29"), new("A15-zC29"), new("ac15/ZC29")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalMixedDualTokenFormulas_Inequal() {
+        Formula[] testedFormulas = [new("ab12 - .54"), new("12-d3"), new("aB12 / 5.4e-2")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+
+
+
+    // - Tests with two term formulas with parentheses that are not equal -
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualIntegerTokenFormulasWithParens_Inequal() {
+        Formula f1 = new("(((89)) + 32)");
+        Formula f2 = new("( (89)  + 32  )");
+
+        Assert.IsFalse(f1.Equals(f2));
+        Assert.IsFalse(f1 == f2);
+        Assert.IsTrue(f1 != f2);
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualDecimalTokenFormulasWithParens_Inequal() {
+        Formula f1 = new("( ((  (( (32.451))) ))) - ( ((( ((.2))  )) ))");
+        Formula f2 = new("((((((32.45))  )))) -((((.2))) )");
+
+        Assert.IsFalse(f1.Equals(f2));
+        Assert.IsFalse(f1 == f2);
+        Assert.IsTrue(f1 != f2);
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualScientificTokenFormulasWithParens_Inequal() {
+        Formula[] testedFormulas = [new("89/23"), new("  500 / 30"), new("(5E2   )-3E1")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalDualVariableTokenFormulasWithParens_Inequal() {
+        Formula[] testedFormulas = [new("( (a15 - zc29)  )"), new("A15-Zc29"), new("(((A15-zCf29)))"), new("( ( (a15-ZC29)  ))")];
+
+        for (int i = 0; i < testedFormulas.Length; i++) {
+            Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
+            Assert.IsFalse(testedFormulas[i] == testedFormulas[(i + 1) % 3]);
+            Assert.IsTrue(testedFormulas[i] != testedFormulas[(i + 1) % 3]);
+        }
+    }
+
+    [TestMethod]
+    public void FormulaEquivilency_InequalMixedDualTokenFormulasWithParens_Inequal() {
+        Formula[] testedFormulas = [
+            new("( ((5632.0) / 2e4  ))"), 
+            new("(((ad34)/ 2000) )"), 
+            new("( ( (5632000e-3) / 20E2) )"),
+            new("5632 / 02000")
+        ];
 
         for (int i = 0; i < testedFormulas.Length; i++) {
             Assert.IsFalse(testedFormulas[i].Equals(testedFormulas[(i + 1) % 3]));
