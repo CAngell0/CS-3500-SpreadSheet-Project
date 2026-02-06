21af1a67380a5ba3a5d47ae4857ad15ae8b90139
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-06 12:31:25 -0700

    Added stub class for hash code unit tests

---
+++ Spreadsheet/FormulaTests/FormulaHashCodeTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Company.TestProject1;
+
+[TestClass]
+public class FormulaHashCodeTests {
+    [TestMethod]
+    public void TestMethod1() {
+    }
+}
