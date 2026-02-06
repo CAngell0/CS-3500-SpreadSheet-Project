6031f332dae790fe027da85fd5dfa6a89bede866
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-06 13:21:56 -0700

    Added unit tests for GetHashCode that test slightly different dual token formulas

--- Spreadsheet/FormulaTests/FormulaHashCodeTests.cs
+++ Spreadsheet/FormulaTests/FormulaHashCodeTests.cs
@@ -66,4 +66,68 @@ public class FormulaHashCodeTests {
             Assert.AreNotEqual(formulas[i].GetHashCode(), formulas[(i + 1) % len].GetHashCode());
         }
     }
+
+
+
+
+    // - Tests with slighly different dual token formulas -
+
+    [TestMethod]
+    public void FormulaGetHashCode_SlightlyDifferentDualIntegerTokens_DifferentHashCodes() {
+        Formula[] formulas = [
+            new("123 + 456"),
+            new("123 - 456"),
+            new("124 + 456"),
+            new("123 + 457")
+        ];
+
+        int len = formulas.Length;
+        for (int i = 0; i < formulas.Length; i++) {
+            Assert.AreNotEqual(formulas[i].GetHashCode(), formulas[(i + 1) % len].GetHashCode());
+        }
+    }
+
+    [TestMethod]
+    public void FormulaGetHashCode_SlightlyDifferentDualDecimalTokens_DifferentHashCodes() {
+        Formula[] formulas = [
+            new("46.32 * 46.33"),
+            new("46.32e1 * 463.3"),
+            new("46.32 * 46.34"),
+            new("46.32 / 46.33")
+        ];
+
+        int len = formulas.Length;
+        for (int i = 0; i < formulas.Length; i++) {
+            Assert.AreNotEqual(formulas[i].GetHashCode(), formulas[(i + 1) % len].GetHashCode());
+        }
+    }
+
+    [TestMethod]
+    public void FormulaGetHashCode_SlightlyDifferentDualScientificTokens_DifferentHashCodes() {
+        Formula[] formulas = [
+            new("2e3 / 10"),
+            new("2E3 / 11"),
+            new("2e2 / 10"),
+            new("2001 / 10")
+        ];
+
+        int len = formulas.Length;
+        for (int i = 0; i < formulas.Length; i++) {
+            Assert.AreNotEqual(formulas[i].GetHashCode(), formulas[(i + 1) % len].GetHashCode());
+        }
+    }
+    
+    [TestMethod]
+    public void FormulaGetHashCode_SlightlyDifferentDualVariableTokens_DifferentHashCodes() {
+        Formula[] formulas = [
+            new("abc5 + def2"),
+            new("abc6 + def3"),
+            new("ab5 + def2")
+        ];
+
+        int len = formulas.Length;
+        for (int i = 0; i < formulas.Length; i++) {
+            Assert.AreNotEqual(formulas[i].GetHashCode(), formulas[(i + 1) % len].GetHashCode());
+        }
+    }
 }
