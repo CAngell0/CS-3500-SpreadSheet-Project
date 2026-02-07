9726fdd29e60399c3196fca78999449f0a9b32d0
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-06 18:33:29 -0700

    Created stub class for evaluate method tests

---
+++ Spreadsheet/FormulaTests/FormulaEvaluateTests.cs
@@ -0,0 +1,11 @@
+// <author> Carson ANgell </author>
+// <date> 2/6/2026 </date>
+
+namespace FormulaTests;
+
+[TestClass]
+public class FormulaEvaluateTests {
+    [TestMethod]
+    public void TestMethod1() {
+    }
+}
