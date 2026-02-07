2b7a2934e57e998742126651b78b542f51722940
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-06 18:39:09 -0700

    Created method that define the lookup delegate before each test

--- Spreadsheet/FormulaTests/FormulaEvaluateTests.cs
+++ Spreadsheet/FormulaTests/FormulaEvaluateTests.cs
@@ -1,11 +1,22 @@
 // <author> Carson ANgell </author>
 // <date> 2/6/2026 </date>
 
+using Formula;
+
 namespace FormulaTests;
 
 [TestClass]
 public class FormulaEvaluateTests {
-    [TestMethod]
-    public void TestMethod1() {
+    [TestInitialize]
+    public void DefineLookupMethod() {
+        Lookup lookup = (variable) => {
+            return variable switch {
+                "A1" => 10,
+                "B1" => 20,
+                "C1" => 100,
+                "D1" => 30.6,
+                _ => throw new ArgumentException(),
+            };
+        };
     }
 }
