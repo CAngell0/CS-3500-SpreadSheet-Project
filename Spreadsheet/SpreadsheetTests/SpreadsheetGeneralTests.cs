023fdd68f7dd11d45f8517b7b604534e3dadf631
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-02-09 23:04:00 -0700

    Added some tests for setting cell contents

--- Spreadsheet/SpreadsheetTests/SpreadsheetGeneralTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetGeneralTests.cs
@@ -8,6 +8,63 @@ using Formula;
 
 [TestClass]
 public sealed class SpreadSheetGeneralTests {
+    // --- SetCellContents method tests
+    [TestMethod]
+    public void SpreadsheetSetCellContents_OfEmptyStringOnEmptySheet_InvalidNameException() {
+        Spreadsheet spreadsheet = new();
+        Assert.Throws<InvalidNameException>(() => spreadsheet.SetCellContents("", 5));
+    }
+
+    [TestMethod]
+    public void SpreadsheetSetCellContents_InvalidCellNameOnEmptySheet_InvalidNameException() {
+        Spreadsheet spreadsheet = new();
+        Assert.Throws<InvalidNameException>(() => spreadsheet.SetCellContents("H", 5));
+        Assert.Throws<InvalidNameException>(() => spreadsheet.SetCellContents("8", 5));
+    }
+
+    [TestMethod]
+    public void SpreadsheetSetCellContents_SettingExistingDoubleCell_InvalidNameException() {
+        Spreadsheet spreadsheet = new();
+        spreadsheet.SetCellContents("A5", 5);
+        spreadsheet.SetCellContents("A5", 59);
+
+        object result = spreadsheet.GetCellContents("A5");
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<double>(result);
+        Assert.AreEqual(59, (double) result);
+    }
+
+    [TestMethod]
+    public void SpreadsheetSetCellContents_SettingExistingStringCell_InvalidNameException() {
+        Spreadsheet spreadsheet = new();
+        spreadsheet.SetCellContents("A5", "Hello");
+        spreadsheet.SetCellContents("A5", "World");
+
+        object result = spreadsheet.GetCellContents("A5");
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<string>(result);
+        Assert.AreEqual("World", (string) result);
+    }
+
+    [TestMethod]
+    public void SpreadsheetSetCellContents_SettingExistingFormulaCell_InvalidNameException() {
+        Spreadsheet spreadsheet = new();
+        spreadsheet.SetCellContents("A5", "3 * 80");
+        spreadsheet.SetCellContents("A5", "2e2 / 5");
+
+        object result = spreadsheet.GetCellContents("A5");
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<string>(result);
+        Assert.IsTrue(((Formula) result).Equals(new Formula("200/5")));
+    }
+
+
+
+
+
     // --- GetCellContents method tests
     [TestMethod]
     public void SpreadsheetGetCellContents_OfEmptyString_InvalidNameException() {
